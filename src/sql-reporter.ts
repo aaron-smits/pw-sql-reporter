bc7cfb261ed57c2b33f2831c93af3db72dfafc50
aaron <aaronsmitstc@gmail.com>
2024-03-23 16:32:59 -0500

    Update dependencies and remove unused code

--- src/sql-reporter.ts
+++ src/sql-reporter.ts
@@ -1,36 +1,35 @@
 import type {
-    FullResult,
-    TestCase,
-    TestResult,
-    Reporter
-} from "@playwright/test/reporter"
-import { TursoDB } from "./database/turso"
-import { Database } from "./database/database-interface"
+  FullResult,
+  TestCase,
+  TestResult,
+  Reporter,
+} from "@playwright/test/reporter";
+import { TursoDB } from "./turso";
+import { Database } from "./database-interface";
 
 export default class SqlReporter implements Reporter {
-    private db: Database
-    constructor(options: { db?: Database, dropTables?: boolean } = {}) {
-        this.db = options.db || new TursoDB()
-        if (options.dropTables) {
-            this.db.dropTables()
-        }
+  private db: Database;
+  constructor(options: { db?: Database; dropTables?: boolean } = {}) {
+    this.db = options.db || new TursoDB();
+    if (options.dropTables) {
+      this.db.dropTables();
     }
-    async onBegin() {
-        // await this.db.dropTables()
-        await this.db.createTables()
-        await this.db.createTestRun()
-    }
+  }
+  async onBegin() {
+    // await this.db.dropTables()
+    await this.db.createTables();
+    await this.db.createTestRun();
+  }
 
-    async onTestBegin(test: TestCase) {
-        await this.db.createTest(test)
-    }
+  async onTestBegin(test: TestCase) {
+    await this.db.createTest(test);
+  }
 
-    async onTestEnd(test: TestCase, result: TestResult) {
-        await this.db.updateTest(test, result)
-    }
+  async onTestEnd(test: TestCase, result: TestResult) {
+    await this.db.updateTest(test, result);
+  }
 
-    async onEnd(result: FullResult) {
-        await this.db.updateTestRun(result)
-    }
+  async onEnd(result: FullResult) {
+    await this.db.updateTestRun(result);
+  }
 }
-
